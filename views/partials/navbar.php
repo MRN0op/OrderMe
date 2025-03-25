f9b46a206027a6f2b5f243e785561047479e8040
spider845 <gonnerloris845@gmail.com>
2025-03-25 10:28:29 +0100

    Added Logo to the Nav

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -3,7 +3,11 @@
         <div class="flex justify-between items-center h-16">
             <!-- Logo -->
             <div class="flex-shrink-0">
-                <a href="/" class="text-2xl font-extrabold text-gray-800">OrderMe</a>
+                <a href="/" class="text-4xl font-extrabold flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 transition-colors duration-300 ease-in-out hover:fill-blue-600" viewBox="0 -960 960 960" fill="currentColor">
+                        <path d="M280-160q-50 0-85-35t-35-85H60l18-80h113q17-19 40-29.5t49-10.5q26 0 49 10.5t40 29.5h167l84-360H182l4-17q6-28 27.5-45.5T264-800h456l-37 160h117l120 160-40 200h-80q0 50-35 85t-85 35q-50 0-85-35t-35-85H400q0 50-35 85t-85 35Zm357-280h193l4-21-74-99h-95l-28 120Zm-19-273 2-7-84 360 2-7 34-146 46-200ZM20-427l20-80h220l-20 80H20Zm80-146 20-80h260l-20 80H100Zm180 333q17 0 28.5-11.5T320-280q0-17-11.5-28.5T280-320q-17 0-28.5 11.5T240-280q0 17 11.5 28.5T280-240Zm400 0q17 0 28.5-11.5T720-280q0-17-11.5-28.5T680-320q-17 0-28.5 11.5T640-280q0 17 11.5 28.5T680-240Z" />
+                    </svg>
+                </a>
             </div>
 
             <!-- Desktop Navigation Links (visible on lg and above) -->
@@ -55,7 +59,7 @@
                 <?php if (isset($_SESSION['user_type'])): ?>
 
                     <a href="/logout" class="border border-blue-600 text-blue-600 px-5 py-2 rounded-full btn-hover">Logout</a>
-                    
+
                 <?php else: ?>
 
                     <div class="relative group">

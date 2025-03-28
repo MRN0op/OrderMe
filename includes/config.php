809a7373fb244dbdcc43076d6472762841f73081
spider845 <gonnerloris845@gmail.com>
2025-03-28 12:27:23 +0100

    dd

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,9 @@
 <?php
 return [
     'db' => [
-<<<<<<< HEAD
         'host' => '127.0.0.1',
         'username' => 'root',
         'password' => '',
-=======
->>>>>>> 44f31d9703f5c42fd686e5b1b5e4e5a7ad6bcc67
         'database' => 'OrderMe',
         'port' => 3306,
     ],
@@ -19,12 +16,5 @@ return [
         '/dashboard' => 'controllers/dashboard.php',
         '/api/v1/getDeliveryAgents' => 'controllers/api/v1/getDeliveryAgents.api.php',
         '/api/v1/verifyLogin' => 'controllers/api/v1/verifyLogin.api.php',
-
-<<<<<<< HEAD
-=======
-        '/api/(\d+)' => '',
-        '/api' => 'controllers/api.php',
-        '/api/verifyLogin' => 'controllers/verifyLogin.api.php',
->>>>>>> 44f31d9703f5c42fd686e5b1b5e4e5a7ad6bcc67
     ],
 ];

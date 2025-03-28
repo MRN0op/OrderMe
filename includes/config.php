350e9425842655fa31f1e6471bd88f98f6b2ee13
spider845 <gonnerloris845@gmail.com>
2025-03-28 12:34:14 +0100

    config

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@ return [
 
         'host' => '127.0.0.1',
         'username' => 'root',
-        'password' => '',
+        'password' => 'mysecretpassword',
         'database' => 'OrderMe',
         'port' => 3306,
     ],
